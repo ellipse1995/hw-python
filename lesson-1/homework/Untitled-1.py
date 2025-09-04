83aae612e3eea2d093f8f93a970381b383fbd265
ellipse1995 <155900820+ellipse1995@users.noreply.github.com>
2025-09-04 11:20:37 +0500

    Add files via upload

---
+++ lesson-1/homework/Untitled-1.py
@@ -0,0 +1,43 @@
+# %%
+#Given a side of square. Find its perimeter and area.
+
+a = int(input('Give a side of square'))
+print(f'Square with a side {a} sm, has {a*4} sm perimeter and {a**2} sm2 area')
+
+
+# %%
+#Given diameter of circle. Find its length.
+a=int(input('Give a diameter of circle'))
+print(f'Circle with a diameter {a} sm, has {a*3.14} sm of length')
+
+
+# %%
+#Given two numbers a and b. Find their mean.
+a=int(input('Give a first number'))
+b=int(input('Give a second number'))
+print(f'You have written {a} as a first number and {b} as a second number.')
+print(f'The mean of these numbers is equal {(a+b)/2}')
+
+# %%
+#Given two numbers a and b. Find their sum, product and square of each number.
+a=int(input('Give a first number'))
+b=int(input('Give a second number'))
+print(f'You have written {a} as a first number and {b} as a second number.')
+print('Sum of these numbers is',a+b)
+print('Product of these numbers is',a*b)
+print('Square of the first number is',a**2)
+print('Square of the second number is',b**2)
+
+# %%
+
+
+# %%
+
+
+# %%
+
+
+# %%
+
+
+
